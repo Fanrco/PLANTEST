8231d82df0bb284e16d86b99be4c759f4252b21f
Fanrco <38868891+Fanrco@users.noreply.github.com>
2020-04-25 11:19:58 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+    cnv = createCanvas(600,400);
+    cnv.parent("cnv");
+    background(91,191,0);
+    
+
+}
+
+function draw(){
+    
+}
+
+function mousePressed(){
+    
+}
+
+function downloadGarden(){
+    document.body.innerHTML+="<a id='test' href='data:text;charset=utf-8,"+encodeURIComponent("hi")+"' download='GardenSave'>Your Download</a>";
+    document.getElementById('test').click();
+}
